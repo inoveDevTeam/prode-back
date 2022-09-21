222ed65fa730b17093dc40b8071f1ae6f9b19ae3
hernancontigiani <hernan4790@gmail.com>
2022-09-21 08:02:44 -0300

    db config

--- prode/prode/settings.py
+++ prode/prode/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'prode.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '../db.sqlite3',
     }
 }
 
