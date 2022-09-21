a4c5789345ee78ea491e70ce9bb2d1208604020d
hernancontigiani <hernan4790@gmail.com>
2022-09-21 12:03:46 -0300

    Quitar autenticacion basica

--- prode/prode/settings.py
+++ prode/prode/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
        # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
