66573d690a331c0a8e0c49a7e319fcb65c6f5ab8
Hernan Contigiani <61872731+hernancontigiani@users.noreply.github.com>
2022-09-22 18:39:19 -0300

    Modelos (#3)
    * Falta add token
    
    * review (#2)
    
    Co-authored-by: JohanaRG <johanarangeldo@gmail.com>

--- prode/apuestas/models.py
+++ prode/apuestas/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+# NOTE: Para poder utilizar el modelo "user" que viene por defecto en Django,
+# Debemos importarlo previamente:
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Torneo(models.Model):
+    '''Esta clase hereda de Django models.Model y crea una tabla llamada
+    torneo'''
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=60, default='')
+   
+    class Meta:
+        db_table = 'torneo'
+            
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Equipo(models.Model):
+    '''Esta clase hereda de Django models.Model y crea una tabla llamada
+    equipo'''
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=60)
+   
+    class Meta:
+        db_table = 'equipo'
+            
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Partido(models.Model):
+    '''Esta clase hereda de Django models.Model y crea una tabla llamada
+    partido'''
+
+    id = models.BigAutoField(primary_key=True)
+    torneo = models.ForeignKey(Torneo, on_delete=models.CASCADE)
+    equipo_1 = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='equipo1_set')
+    equipo_2 = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='equipo2_set')
+    fecha = models.DateTimeField()
+    descripcion = models.CharField(max_length=60, default='', blank=True)
+    cerrado = models.BooleanField(default=False, blank=True)
+    resultado_equipo_1 = models.PositiveIntegerField(default=None, blank=True, null=True)
+    resultado_equipo_2 = models.PositiveIntegerField(default=None, blank=True, null=True)
+    terminado =  models.BooleanField(default=False, blank=True)
+
+    class Meta:
+        db_table = 'partido'
+            
+    def __str__(self):
+        return f'{self.id} {self.torneo} {self.equipo_1} {self.equipo_2} {self.resultado_equipo_1} {self.resultado_equipo_2}'
+
+
+class Pronostico(models.Model):
+    '''Esta clase hereda de Django models.Model y crea una tabla llamada
+    pronostico'''
+
+    id = models.BigAutoField(primary_key = True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
+    pronostico_equipo_1 = models.PositiveIntegerField()
+    pronostico_equipo_2 = models.PositiveIntegerField()
+    puntaje = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'pronostico'
+            
+    def __str__(self):
+        return f'{self.id} {self.partido_id} {self.pronostico_equipo_1} {self.pronostico_equipo_2} {self.puntaje}'
