b31acdde9f3b7efa3c3e3c88a3a75142d55a1497
hernancontigiani <hernan4790@gmail.com>
2022-09-23 08:11:35 -0300

    Mock datos mundial

---
+++ mock_data/mock_mundial.py
@@ -0,0 +1,41 @@
+import os
+import csv
+from apuestas.models import *
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import datetime
+
+
+with open("../mock_data/mundial.csv") as fi:
+    data = list(csv.DictReader(fi))
+
+hernan = User.objects.create_user("hernan", password="prode1234")
+johana = User.objects.create_user("johana", password="prode1234")
+santiago = User.objects.create_user("santiago", password="prode1234")
+
+torneo = Torneo.objects.create(name="Mundial Qatar 2022")
+
+equipos = []
+for row in data:
+    equipos.append(row["equipo1"].capitalize())
+    equipos.append(row["equipo2"].capitalize())
+
+equipos = sorted(list(set(equipos)))
+equipos_db = {}
+for equipo in equipos:
+    equipos_db[equipo] = Equipo.objects.create(name=equipo)
+
+meses = {"nov": 11, "dic": 12}
+
+for row in data:
+    dia = int(row["fecha"].split("-")[0])
+    mes = meses[row["fecha"].split("-")[1]]
+    hora = int(row["hora"])
+    fecha = timezone.make_aware(datetime(2022, mes, dia, hora))
+    Partido.objects.create(
+        torneo = torneo,
+        equipo_1 = equipos_db[row["equipo1"].capitalize()],
+        equipo_2 = equipos_db[row["equipo2"].capitalize()],
+        fecha = fecha,
+        descripcion=f'Grupo {row["grupo"]}'
+    )
