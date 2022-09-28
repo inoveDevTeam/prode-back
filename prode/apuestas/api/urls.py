9cee6348c4c8da2f2ad0961b5440bef61236f92f
RGJohana <j0hana.rangel.ga@gmail.com>
2022-09-28 00:45:13 -0300

    Apis entrega1

--- prode/apuestas/api/urls.py
+++ prode/apuestas/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path(f'login/', LoginUserAPIView.as_view(), name="login"),
     path(f'partidos/', PartidosAPIView.as_view(), name="partidos"),
     path(f'pronosticos/', PronosticosAPIView.as_view(), name="pronosticos"),
-    path(f'pronosticos/<int:pk>/<int:t1>/<int:t2>', PronosticosIdAPIView.as_view(), name="actualizar"),
+    path(f'pronosticos/<int:pk>/<int:t1>/<int:t2>/', PronosticosIdAPIView.as_view(), name="actualizar"),
     path(f'pronosticos/<user>/<e1>/<e2>/<int:p1>/<int:p2>/<int:punto>/', PronosticosApustaAPIView.as_view(), name="apuesta"),
 
 
