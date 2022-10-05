3267e05650d9e3ddb28e50f72503f55ed3e9b865
hernancontigiani <hernan4790@gmail.com>
2022-10-05 20:18:00 -0300

    Test para ranking

---
+++ test/ranking_get.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+token = "c52ec8d3fdbfdaf776b869a41fb7c5de48592c5d"
+headers = {
+    'Authorization': f'Token {token}',
+    "Content-Type": "application/json"
+}
+
+url = "http://127.0.0.1:8000/api/v1.0/ranking/"
+
+resp = requests.get(url, headers=headers)
+print(resp.status_code)
+print(resp.json())
