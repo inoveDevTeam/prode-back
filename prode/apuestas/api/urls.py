4e8e064fe42536d9a73e18cf171fd1430cd15934
hernancontigiani <hernan4790@gmail.com>
2022-10-05 20:10:32 -0300

    Agregar endpoint ranking

--- prode/apuestas/api/urls.py
+++ prode/apuestas/api/urls.py
@@ -5,5 +5,5 @@ from .api_views import *
 urlpatterns = [
     path(f'login/', LoginUserAPIView.as_view(), name="login"),
     path(f'partidos/pronosticos/', PartidosPronosticosAPIView.as_view(), name="partidos"),
-
+    path(f'ranking/', RankingAPIView.as_view(), name="ranking"),
 ]
