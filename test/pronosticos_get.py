d19cab02ed943e1ec86dd0b7cbb21d6ffe2429d0
hernancontigiani <hernan4790@gmail.com>
2022-10-05 20:15:59 -0300

    Ajustar tests

--- test/pronosticos_get.py
+++ test/pronosticos_get.py
@@ -1,13 +1,13 @@
 import json
 import requests
 
-token = "ce8aec0a47ee26b03f2fe7f4038ba392bba95ed9"
+token = "c52ec8d3fdbfdaf776b869a41fb7c5de48592c5d"
 headers = {
     'Authorization': f'Token {token}',
     "Content-Type": "application/json"
 }
 
-url = "http://127.0.0.1:8000/api/v1.0/pronosticos/"
+url = "http://127.0.0.1:8000/api/v1.0/partidos/pronosticos/"
 
 resp = requests.get(url, headers=headers)
 print(resp.status_code)
