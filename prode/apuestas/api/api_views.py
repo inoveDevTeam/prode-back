6f8e94a7f183961986351e520c0c324b3f9ef0fa
Hernan Contigiani <61872731+hernancontigiani@users.noreply.github.com>
2022-10-06 18:42:08 -0300

    Api codereview (#7)
    * Mover de lugar de donde se generar los datos
    
    * Cambiar datos segun requerimiento de santi (incluido el formato de la fecha)
    
    * Ajustar tests
    
    * Api codereview ranking (#8)
    
    * Agregar API de rankings
    
    * Agregar endpoint ranking
    
    * Test para ranking
    
    * Actualizar tokens
    
    * Agregar metodo Sum para el ranking

--- prode/apuestas/api/api_views.py
+++ prode/apuestas/api/api_views.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import locale
+
 # Primero, importamos los serializadores
 #from apuestas.api.serializers import *
 
@@ -27,6 +29,7 @@ from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 
 from django.utils import timezone
+from django.db.models import Sum
 
 
 class LoginUserAPIView(APIView):
@@ -90,8 +93,7 @@ class PartidosPronosticosAPIView(APIView):
             # El usuario lo sacamos del request, 
             user = request.user  
 
-            partidos_disponibles = []
-            partidos_apostados = []
+            data = []
             
             # Buscar todas los partidos
             datos_partidos = Partido.objects.all().order_by("fecha")
@@ -99,68 +101,47 @@ class PartidosPronosticosAPIView(APIView):
             # Buscar los pronosticos del user
             pronosticos = Pronostico.objects.filter(usuario=user).order_by("partido__fecha")
 
+            locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
+
             # Recorre los partidos guardados
             for match in datos_partidos:
+
+                if match.cerrado == False and match.terminado == False:
+                    estado = 0
+                elif match.cerrado == True and match.terminado == False:
+                    estado = 1
+                else:
+                    estado = 2
+
+                dict_partidos = {
+                    "partido_id": match.id,
+                    "torneo_name": match.torneo.name,
+                    "equipo_1": match.equipo_1.name,
+                    "equipo_2": match.equipo_2.name,
+                    "fecha": timezone.localtime(match.fecha).strftime('%A %d, %B %Y %H:%M'),
+                    "descripcion": match.descripcion,
+                    "estado": estado,
+                    "resultado_equipo_1": match.resultado_equipo_1,
+                    "resultado_equipo_2": match.resultado_equipo_2,
+                    # Se muestra que no está apostado
+                    "pronostico_equipo_1": None,
+                    "pronostico_equipo_2": None,
+                    "puntaje": 0
+                    }
+
                 for pronostico in pronosticos: # Recorre los pronosticos guardados del user
                     partido_forecast = pronostico.partido # Patidos pronosticados por el user
-                
+
                     if match.id == partido_forecast.id: # Si el nro de partido es igual al partido apostado
-                        # Guardará en una lista de diccionario todos los partidos apostados y sino muestra los 
-                        # Partidos disponibles (no apostados) 
+                        # Actualiza en los datos los campos apostados por el usuario
+                        dict_partidos["pronostico_equipo_1"] = pronostico.pronostico_equipo_1,
+                        dict_partidos["pronostico_equipo_2" ] = pronostico.pronostico_equipo_2,
+                        dict_partidos["puntaje"] = pronostico.puntaje
+                        break
 
-                        if partido_forecast.cerrado == False and partido_forecast.terminado == False:
-                            estado = 0
-                        elif partido_forecast.cerrado == True and partido_forecast.terminado == False:
-                            estado = 1
-                        else:
-                            estado = 2
+                data.append(dict_partidos)
 
-                        pronostico_user = {
-                            "partido_id": partido_forecast.id,
-                            "torneo_name": partido_forecast.torneo.name,
-                            "equipo_1_name": partido_forecast.equipo_1.name,
-                            "equipo_2_name": partido_forecast.equipo_2.name,
-                            "fecha": partido_forecast.fecha,
-                            "descripcion": partido_forecast.descripcion,
-                            "estado": estado,
-                            "resultado_equipo_1": partido_forecast.resultado_equipo_1,
-                            "resultado_equipo_2": partido_forecast.resultado_equipo_2,
-                            "pronostico_equipo_1": pronostico.pronostico_equipo_1,
-                            "pronostico_equipo_2": pronostico.pronostico_equipo_2,
-                            "puntaje": pronostico.puntaje
-                            }
-
-                        partidos_apostados.append(pronostico_user)
-                        
-
-                    else:
-                        
-                        dict_partidos = {
-                            "partido": match.id,
-                            "torneo_name": match.torneo.name, 
-                            "equipo_1": match.equipo_1.name,
-                            "equipo_2": match.equipo_2.name,
-                            "fecha": match.fecha,
-                            "descripcion": match.descripcion,
-                            "cerrado": match.cerrado,
-                            "resultado_equipo_1": match.resultado_equipo_1,
-                            "resultado_equipo_2": match.resultado_equipo_2,
-                            "terminado": match.terminado,
-                            # Se muestra que no está apostado 
-                            "pronostico_equipo_1": None,
-                            "pronostico_equipo_2": None,
-                            "puntaje": 0             
-                            }
-
-                        if dict_partidos in partidos_disponibles:
-                            pass
-                        else:
-                            partidos_disponibles.append(dict_partidos)
-
-            # print('apostados', partidos_apostados)
-            # print('disponibles', partidos_disponibles)                                                              
-           
-            return JsonResponse({'data':['apostados', partidos_apostados, 'disponibles', partidos_disponibles]}, status=status.HTTP_200_OK, safe=False)
+            return JsonResponse({"data": data}, status=status.HTTP_200_OK)
         
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -188,6 +169,43 @@ class PartidosPronosticosAPIView(APIView):
 
            
             return JsonResponse(data={}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(f"{fname} {exc_tb.tb_lineno} {e}") # esto lo vamos a mejorar con logging
+            # HC --> ya que no retornamos ninguna información de valor,
+            # no retornemos ninguna (hay que investigar como mejorar esto)
+            return JsonResponse(data={}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RankingAPIView(APIView): 
+    parser_classes = (JSONParser,)
+    
+    def get(self, request):
+        """ Retorna la lista de mis apuestas(por usuario) con sus puntajes"""
+        try:
+            user = request.user
+
+            ranking = Pronostico.objects.values("usuario__username", "usuario__first_name", "usuario__last_name").annotate(puntaje_total=Sum('puntaje')).order_by("-puntaje_total")
+
+            data = {"puntaje": 0, "posicion": None, "ranking": []}
+            for i, pos in enumerate(ranking):
+                dict_data = {
+                    "username": pos["usuario__username"],
+                    "posicion": i+1,
+                    "first_name": pos["usuario__first_name"],
+                    "last_name": pos["usuario__last_name"],
+                    "puntaje_total": pos["puntaje_total"]
+                }
+                data["ranking"].append(dict_data)
+
+                if pos["usuario__username"] == user.username:
+                    print(pos)
+                    data["puntaje"] = pos["puntaje_total"]
+                    data["posicion"] = i+1
+
+            return JsonResponse(data, status=status.HTTP_200_OK)
 
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
