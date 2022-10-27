d95ceb6f98c86393e67e574d7cbcc43e68b00b01
hernancontigiani <hernan4790@gmail.com>
2022-10-27 19:00:05 -0300

    Pruebas 27-octubre

--- prode/apuestas/tests.py
+++ prode/apuestas/tests.py
@@ -2,50 +2,53 @@ from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework import status
 # ---
-from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 from .models import *
-# from django.test import Client
 
-  
+from django.urls import reverse
 
 class TestLoginAPIViewTestCase(APITestCase):
     
     def setUp(self):
         '''Inicialización de la clase'''
 
-        # La ruta de login (tambien se puede usar revere())
-        self.login_url = '/login/'
+        self.url = reverse('login')
+        self.url2 = reverse('partidos')
 
         # Se crea el usuario
-        self.user = User(
+        self.user = User.objects.create_user(
                         username="test", 
                         password="prode1234"
                         )
-        self.user.save()
+        return super().setUp()
 
-        # Para realizar las peticiones, viene incorporada el apitestcase y no hay que definirla
-        # post recibe la ruta, la data que se le va a enviar y el formato
+    def test_login(self):
         response = self.client.post(
-            self.login_url,
+        self.url,
             {
                 'username':"test",
                 'password':"prode1234"
             },
             format='json'
         )
-
         self.assertEqual(response.status_code, status.HTTP_201_CREATED) # El código de respuesta cuando se crea un user es 201
-        
-        #self.token = response.data['token'] # sino funciona usar user_data en vez de data
-        # A esta instancia setear la autorización con el token
-        #self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
-        return super().setUp()
 
-    def test_login(self):
-        pass
-        # print(self.token)
+        # Tomar el token para un proximo request        
+        print(response.json())
+        token = response.json()["token"]
 
+        # Utilizar el token para autenticarnos
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+
+        # Relealizar una nueva peticion
+        response = self.client.get(
+            path=self.url2,
+            data={
+                'username':"test",
+                'password':"prode1234"
+            },
+            format='json',
+        )
+        print(response.json())
 
 
 # class TestAPIViewTestCase(APITestCase):
